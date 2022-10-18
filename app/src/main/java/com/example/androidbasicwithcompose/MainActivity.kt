98a972f283478301bd87304ccb8bb55ea741579a
A-fauzi <akhmadfauzi0101@gmail.com>
2022-10-18 18:14:40 +0700

    Unit 1: Your first Android app | Build a basic layout -> Build an Android app with a simple user interface that displays text and images.

--- app/src/main/java/com/example/androidbasicwithcompose/MainActivity.kt
+++ app/src/main/java/com/example/androidbasicwithcompose/MainActivity.kt
@@ -3,16 +3,21 @@ package com.example.androidbasicwithcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androidbasicwithcompose.ui.theme.AndroidBasicWithComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
             AndroidBasicWithComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Akhmad Fauzi")
+                    BirthdayGreetingWithImage(stringResource(R.string.happy_birthday_saying), "Nanda")
                 }
             }
         }
@@ -30,9 +35,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Surface(color = Color.Magenta) {
-        Text(text = "Hi, My name is $name!", Modifier.padding(24.dp))
+fun BirthdayGreetingWithImage(message: String, from: String) {
+    val image = painterResource(id = R.drawable.androidparty)
+
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        Saying(name = message, sender = from)
+    }
+}
+
+
+@Composable
+fun Saying(name: String, sender: String) {
+    Column(
+        modifier = Modifier
+            .padding(top = 16.dp)
+    ) {
+        Text(
+            text = "$name!",
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+        )
+
+        Text(
+            text = "- From $sender",
+            fontSize = 12.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+        )
     }
 }
 
@@ -40,6 +79,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     AndroidBasicWithComposeTheme {
-        Greeting("Akhmad Fauzi")
+        BirthdayGreetingWithImage(stringResource(R.string.happy_birthday_saying), "Nanda")
     }
 }
